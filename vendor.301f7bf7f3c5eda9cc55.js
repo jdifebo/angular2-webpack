7685316874cb00a5cdced75d3e83397788833672
Joe DiFebo <jdifebo@nexient.com>
2016-09-18 21:42:21 -0400

    changed webpack settings to make demo work

--- vendor.301f7bf7f3c5eda9cc55.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],[function(c,n){"use strict"}]);
-//# sourceMappingURL=vendor.301f7bf7f3c5eda9cc55.js.map
---
+++ vendor.3313b93faf6f1dd2601f.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],[function(c,n){"use strict"}]);
+//# sourceMappingURL=vendor.3313b93faf6f1dd2601f.js.map
